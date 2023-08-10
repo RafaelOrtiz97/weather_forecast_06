f111098aaac63037fca8c4596aa42d66deaff4d7
Rafael Ortiz <rafaelortiz.pro@gmail.com>
2023-08-09 17:54:33 -0700

    add html css

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,17 @@ function GetInfo() {
     currentCity.innerHTML = newName;
 }
 
-fetch("http://api.openweathermap.org/geo/1.0/direct?q=newName&limit=5&appid=aacafb400b6eef1cdd04ebdad8f51f57");
+fetch("http://api.openweathermap.org/geo/1.0/direct?q=newName&limit=5&appid=aacafb400b6eef1cdd04ebdad8f51f57")
+.then(response => response.json())
+.then(data => {
+    var lat = data.lat;
+    var lon = data.lon;
+})
 
-fetch("api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=828d79693a4956081486ecac446f61b2");
+fetch("api.openweathermap.org/data/2.5/forecast?lat=lat&lon=lon&appid=828d79693a4956081486ecac446f61b2")
+.then(response => response.json())
+.then(data => {
+    for(i = 0; i < 5; i++){
+        document.querySelectorAll("date")
+    }
+})
